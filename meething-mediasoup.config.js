b59768948a6e39b397246260c645a8851128da68
大月学 <m-ootsuki@yournet-jp.com>
2020-09-08 08:49:16 +0900

    certs

--- meething-mediasoup.config.js
+++ meething-mediasoup.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-  apps : [{
-    name: 'meething-mediasoup',
-    script: 'main.js',
-    env : {
-      SSL : true,
-      SSLKEY : 'certs/privkey.pem',
-      SSLCERT  : 'certs/fullchain.pem',
-      DEBUG : false
-    }
-  }]
+  apps: [
+    {
+      name: 'meething-mediasoup',
+      script: 'main.js',
+      env: {
+        SSL: true,
+        // SSLKEY : 'certs/privkey.pem',
+        // SSLCERT  : 'certs/fullchain.pem',
+        SSLKEY: '../roomtags/certs/privkey.pem',
+        SSLCERT: '../roomtags/certs/fullchain.pem',
+        DEBUG: false,
+      },
+    },
+  ],
 };
